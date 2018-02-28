ec5bd7b90f63d777f29c3574d774ef6d6a7fa2f9
Jimmy Nguyen <jnguyen6@engr-ras-200.eos.ncsu.edu>
2018-02-28 17:06:28 -0500

    Removed function block comments in wordlist.c and display.c

--- p3/display.c
+++ p3/display.c
@@ -26,14 +26,6 @@
 /** The stick figure with all seven body parts. */
 #define FULL_FIGURE 7
 
-/**
- * Prints the characters in the given word array to standard
- * output. Some or all of the characters in the word array
- * can be underscores. A whitespace is used to separate each
- * printed character.
- *
- * @param word the word array to print to standard output
- */
 void displayWord( char word[ ] )
 {
     printf( "%c", word[ 0 ] );
@@ -42,13 +34,6 @@ void displayWord( char word[ ] )
     }
 }
 
-/**
- * Prints the stick figure to standard output based on the given
- * number of body parts. If the given number of body parts is 0,
- * then only a new line character is printed.
- *
- * @param numberOfParts the number of parts to print
- */
 void displayFigure( int numberOfParts )
 {
     if ( numberOfParts == HEAD ) {
